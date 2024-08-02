280ecc09b4a81e115a514b64613cbf4be47d7dc3
Alex Morel <alex092697@outlook.es>
2024-08-02 16:09:25 +0200

    clubCard and ClubPage creation

--- src/Pages/ClubsPage.jsx/ClubsPage.jsx
+++ src/Pages/ClubsPage.jsx/ClubsPage.jsx
@@ -1,8 +1,65 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import ClubCard from "../../components/ClubCard/ClubCard"
+import { Card, Col, Row } from "react-bootstrap"
+
+
+
+const API_URL = "http://localhost:5005"
+
+
 const ClubPage = () => {
 
-    return (
+    const [clubs, setClubs] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
-        <p>Aqui estra ClubPage</p>
-    )
+    useEffect(() => {
+        fetchAllClubs()
+    }, [])
+
+
+    const fetchAllClubs = () => {
+
+        axios
+            .get(`${API_URL}/clubs`)
+            .then((response) => {
+                setClubs(response.data)
+                setIsLoading(false)
+            })
+            .catch((error) => console.log(error))
+
+    }
+
+    return (
+        < div className="ClubsPage" >
+
+            {isLoading ?
+                <h1>Cargando...</h1>
+                : clubs.map(eachClub => {
+
+                    return (
+                        <div key={eachClub.id}>
+                            < Link to={`${API_URL}/clubs/${eachClub.id}`} />
+                            <ClubCard {...eachClub} />
+                        </div>
+                    )
+
+
+                })}
+
+        </div >
+
+
+    );
 }
+
 export default ClubPage
+
+
+
+
+
+
+
--- src/components/ClubCard/ClubCard.jsx
+++ src/components/ClubCard/ClubCard.jsx
@@ -1,9 +1,26 @@
-const ClubCard = () => {
+import { Card, ListGroup, Row } from 'react-bootstrap'
+import './ClubCard.css'
+
+const ClubCard = ({ pictures, name, facilities, city, town, services }) => {
+
+
+
+
 
     return (
-
-        <p>Aqui esta Clib Card</p>
-
+        <Row xs={1} md={2} className="g-4">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img className="imagenCard" variant="top" src="https://imgs.search.brave.com/zLCiAgpLMlcieuGsIUtQCHPHm7Z-R10AK-YV9y4Zb1E/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2x1Ymxhc2FudGEu/Y29tL2FwcC93ZWJy/b290L19yZXNpemVk/L3VwbG9hZHMvcGFn/ZXMvc3BvcnRzL3Jh/Y2tldHNwb3J0cy9w/YWRlbC1jbGFzcy1i/NGQxMTNhNTk0MGY4/MWM1NWQxMGM4OTE3/NmY4OGMxZi5qcGc" />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <ListGroup className="list-group-clubs">
+                        <ListGroup.Item>City: {city}</ListGroup.Item>
+                        <ListGroup.Item>Town: {town}</ListGroup.Item>
+                        <ListGroup.Item>Services: {services.map(elm => ` ${elm}`)}</ListGroup.Item>
+                    </ListGroup>
+                </Card.Body>
+            </Card>
+        </Row>
     )
 }
 export default ClubCard
