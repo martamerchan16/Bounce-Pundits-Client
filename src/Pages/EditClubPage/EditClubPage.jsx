b6dcd56ff15fe5a8103ada118cbbdefe8db01c08
Marta Merchan <martamerchan16@gmail.com>
2024-08-05 12:30:04 +0200

    conflict solved for first push

--- src/Pages/EditClubPage/EditClubPage.jsx
+++ src/Pages/EditClubPage/EditClubPage.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { useNavigate, useParams } from "react-router-dom"
-import EditClubForm from "../../components/EditClubForm/EditClubForm"
-import { Col, Row } from "react-bootstrap"
-
-
-function EditClubPage() {
-
-    return (
-        <div className="EditClubPage" >
-            <Row>
-
-                <Col md={{ span: 8, offset: 2 }}>
-                    <h1 >Edit Club</h1>
-                    <hr />
-                    <EditClubForm />
-                </Col>
-            </Row>
-        </div >
-    )
-
-}
-=======
 import { useState, useEffect } from "react"
 import axios from "axios"
 import { useNavigate, useParams } from "react-router-dom"
@@ -47,5 +22,4 @@ function EditClubPage() {
     )
 
 }
->>>>>>> ab3fc09f49b6530fb5b8952bf5b6ba7ebd968203
 export default EditClubPage
